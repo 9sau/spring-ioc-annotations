17f57453567b6d158ddfad2785ced4cd136edbfa
sj404x <sj404x@us.att.com>
2019-03-03 13:53:01 -0600

    setter injections

--- src/com/saurabh/ioc/TennisCoach.java
+++ src/com/saurabh/ioc/TennisCoach.java
@@ -3,16 +3,29 @@ package com.saurabh.ioc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-//Register bean in srping container with default name as "tennisCoach"
+//Register bean in spring container with default name as "tennisCoach"
 @Component
 public class TennisCoach implements Coach {
 
 	private FortuneService service;
 
-	//Spring will scan for all components that implements Fortune Service 
-	//and injects it in this constructor. Here, it is HappyFortuneService
+	// Spring will scan for all components that implements Fortune Service
+	// and injects it in this constructor. Here, it is HappyFortuneService
+	/*
+	 * @Autowired public TennisCoach(FortuneService service) { this.service =
+	 * service; }
+	 */
+
+	
+	public TennisCoach(){
+		System.out.println("Inside Tennis Coach contructor");
+	}
+	
+	// Spring will scan for all components that implements Fortune Service
+	// and injects it in this constructor. Here, it is HappyFortuneService
 	@Autowired
-	public TennisCoach(FortuneService service) {
+	public void setFortuneService(FortuneService service){
+		System.out.println("Inside setter for Tennis Coach");
 		this.service = service;
 	}
 
