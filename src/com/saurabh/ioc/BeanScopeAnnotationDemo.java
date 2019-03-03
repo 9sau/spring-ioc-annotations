b4cde45804a6cc84ceb5e14c4ee7955df8b5244e
sj404x <sj404x@us.att.com>
2019-03-03 16:30:44 -0600

    singleton bean scope example which is by default

---
+++ src/com/saurabh/ioc/BeanScopeAnnotationDemo.java
@@ -0,0 +1,23 @@
+package com.saurabh.ioc;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeAnnotationDemo {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		Coach coach1 = context.getBean("tennisCoach", Coach.class);
+		Coach coach2 = context.getBean("tennisCoach", Coach.class);
+
+		boolean isSame = coach1 == coach2;
+
+		System.out.println("Is Same: " + isSame);
+		System.out.println("coach1: " + coach1);
+		System.out.println("coach2: " + coach2);
+		
+		context.close();
+
+	}
+
+}
